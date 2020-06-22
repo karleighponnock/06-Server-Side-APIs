873404a4c4775d5cf52997e64a084a11a2ed8e9c
karleighponnock <karleighponnock@gmail.com>
2020-06-21 22:52:40 -0400

    starting to write first API in

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+let request = new XMLHttpRequest
+request.open( "Get", api.openweathermap.org/data/2.5/weather?q={city name}&appid={7a61aeca48fc673374ccf53ae205e4f4})
+request.send();
+request.onload = () {
+    console.log(request);
+    if request.Status === 200 {
+        console.log(JSON.parse(request.response))
+    }else {
+        console.log(`error ${request.status} ${request.statusText}`)
+    }
+}
