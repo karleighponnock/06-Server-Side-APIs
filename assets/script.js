0abf1e1f8b83375dfa94483eb651701781b03468
karleighponnock <karleighponnock@gmail.com>
2020-06-23 20:37:47 -0400

    temp humidity wind speed uv index all appending properly

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,61 @@
-let request = new XMLHttpRequest
-request.open("Get", api.openweathermap.org / data / 2.5 / weather ? q = { cityname } & appid={ 7a61aeca48fc673374ccf53ae205e4f4 })
-request.send();
-request.onload = () {
-    console.log(request);
-    if (request.Status === 200) {
-        console.log(JSON.parse(request.response))
-    } else {
-        console.log(`error ${request.status} ${request.statusText}`)
+$(document).ready(function () {
+
+    function getWeather(city) {
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7a61aeca48fc673374ccf53ae205e4f4";
+
+        //AJAX request
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: "json"
+        }).then(function (response) {
+            var lat = response.coord.lat
+            var lon = response.coord.lon
+            var temp =response.main.temp
+            var humidity =response.main.humidity
+            var speed =response.wind.speed
+            
+
+            console.log(response)
+            $(".city").text(city);
+            
+            $(".temperature").text(temp);
+            $(".humidity").text(humidity);
+            $(".windSpeed").text(speed);
+
+            
+            getUVIndex(lat, lon);
+        })
     }
-}
+    // getWeather(city);
+
+    function getUVIndex(lat, lon) {
+       
+
+        var queryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=7a61aeca48fc673374ccf53ae205e4f4&lat=${lat}&lon=${lon}`;
+        
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            dataType: "json"
+        }).then(function (response) {
+            var uvIndex =response.value
+            $(".uvIndex").text(uvIndex);
+        
+        })
+
+    }
+
+
+
+    $(".btnCity").on("click", function (event) {
+        event.preventDefault();
+        console.log(this)
+        var city = $(this).text()
+        console.log(city)
+        getWeather(city);
+    })
+})
+
